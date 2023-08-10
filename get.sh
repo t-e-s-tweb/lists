5e4812165c8a3cc8b9abeb8b9a0e854fdcfd66c2
t-e-s-tweb <King.Khanrocks.khan@gmail.com>
2023-08-11 01:58:56 +0500

    Update get.sh

--- get.sh
+++ get.sh
@@ -28,32 +28,24 @@ wget -N https://go.dev/dl/go1.20.4.linux-amd64.tar.gz
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.20.4.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 
-git clone https://github.com/SagerNet/sing-box
-cp -r sing-box/* ./
-rm -rf sing-box
+git clone https://github.com/0xERR0R/blocky
+cp -r blocky/* ./
+rm -rf blocky
 
 
-# Define the text to replace
-old_text="unknown"
-new_text="this is the bestest version of them all"
-
-# Find and edit the target file
-find . -type f -name "version.go" -exec sed -i "s/$old_text/$new_text/g" {} +
-
-
-sed -i '/^import/ { N; N; N; s/\(.*\)/&\n\t"github.com\/KimMachineGun\/automemlimit\/memlimit"\n\t_ "go.uber.org\/automaxprocs"/; }' cmd/sing-box/main.go
+sed -i '/^import/ { N; N; N; s/\(.*\)/&\n\t"github.com\/KimMachineGun\/automemlimit\/memlimit"\n\t_ "go.uber.org\/automaxprocs"/; }' main.go
 
 sed -i '/func main/ i\
 \
 func init() {\
 \tmemlimit.SetGoMemLimitWithProvider(memlimit.Limit(128*1024*1024), 0.7);\
-}' cmd/sing-box/main.go
+}' main.go
 
 go get -u go.uber.org/automaxprocs
 go get github.com/KimMachineGun/automemlimit@latest
 
-env GOOS=linux GOARCH=amd64 CGO_ENABLED=0   go build -o sb -trimpath -ldflags "-s -w -buildid=" -tags with_utls,with_quic,with_wireguard,with_utls,with_gvisor,staticOpenssl,staticZlib,staticLibevent ./cmd/sing-box
-env GOOS=linux GOARCH=arm64 CGO_ENABLED=0   go build -o sbarm -trimpath -ldflags "-s -w -buildid=" -tags with_utls,with_quic,with_wireguard,with_utls,with_gvisor,staticOpenssl,staticZlib,staticLibevent ./cmd/sing-box
+env GOOS=linux GOARCH=arm64 CGO_ENABLED=0  go build -o blockyarm64 -trimpath -ldflags "-s -w -buildid=" ./
+env GOOS=linux GOARCH=amd64 CGO_ENABLED=0  go build -o blocky -trimpath -ldflags "-s -w -buildid=" ./
 
-./upx sb
-./upx sbarm
+./upx blocky
+./upx blockyarm64
