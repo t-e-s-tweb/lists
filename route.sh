49c12dc15bb8d2162e0cd8f9173b269902631335
t-e-s-tweb <King.Khanrocks.khan@gmail.com>
2024-10-19 17:33:10 +0500

    Update route.sh

--- route.sh
+++ route.sh
@@ -1,3 +1,3 @@
 curl https://raw.githubusercontent.com/hagezi/dns-blocklists/main/wildcard/ultimate.txt > a.txt
 curl https://raw.githubusercontent.com/hagezi/dns-blocklists/main/wildcard/tif.txt >> a.txt
-sed '/#/d; s/^\*//' a.txt
+sed -i '/#/d; s/^\*//' a.txt
